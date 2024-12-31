adbba4cb80ee4bf1dae767a2b8f15af9dfc04d5f
ataher5620 <ataher.ict@gmail.com>
2025-01-01 00:00:43 +0600

    btns

---
+++ app.js
@@ -0,0 +1,32 @@
+
+let body = document.getElementById('bdy')
+
+
+let btn_red = () => {
+    console.log("bg-red");
+    body.style.backgroundColor='red'
+}
+
+
+
+let btn_green = () =>{
+    console.log("bg-green");
+    body.style.backgroundColor='green'
+}
+
+let btn_blue = () =>{
+    console.log("bg-green");
+    body.style.backgroundColor='blue'
+}
+
+let btn_yellow = () =>{
+    console.log("bg-green");
+    body.style.backgroundColor='yellow'
+}
+
+let btn_pink = () =>{
+    console.log("bg-green");
+    body.style.backgroundColor='pink'
+}
+
+
